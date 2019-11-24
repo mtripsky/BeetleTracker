fd0a24c88c2d0dd3d25f02fd2b1736ce284ee997
Unknown <m.tripsky@gmail.com>
2019-11-24 16:09:31 +0100

    Add login and registration into mvc

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BeetleTracker.Models
+{
+    public class ApplicationUser : MongoIdentityUser
+    { 
+    }
+}
